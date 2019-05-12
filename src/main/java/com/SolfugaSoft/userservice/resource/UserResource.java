3a70032e4dd7683f2b42519bdf25cb2a3f925f5a
Solfuga Soft <solfugasoft@gmail.com>
2019-05-12 21:34:45 +0200

    add UserResources
    add save user function

--- src/main/java/com/SolfugaSoft/userservice/resource/UserResource.java
+++ src/main/java/com/SolfugaSoft/userservice/resource/UserResource.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -33,11 +30,18 @@ public class UserResource {
 
     @GetMapping("/getall")
         public List<User> getAllUsers(){
-        ResponseEntity<List<User>> userResponse = restTemplate.exchange("http://localhost:8300/rest/db/", HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>() {});
+        ResponseEntity<List<User>> userResponse = restTemplate.exchange("http://localhost:8300/rest/db/all", HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>() {});
         List<User> userList = userResponse.getBody();
 
         return userList;
 
     }
 
+    @PostMapping("/saveuser")
+    public List<User> sendUser(@RequestBody final User user){
+        restTemplate.postForObject("http://localhost:8300/rest/db/adduser", user,String.class);
+
+        List<User> users = getAllUsers();
+        return users;
+    }
 }
